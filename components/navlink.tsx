d882f0395b7b5c5c86121ffc96fce5dcbf3847d7
Caleb <2021.cee.jay@gmail.com>
2026-07-11 23:48:31 +0100

    ..

--- components/navlink.tsx
+++ components/navlink.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 type NavlinkProps = {
   children: React.ReactNode;
   href: string;
@@ -5,13 +7,13 @@ type NavlinkProps = {
 
 const Navlink = ({ children, href }: NavlinkProps) => {
   return (
-    <a
+    <Link
       href={href}
       className="text-primary text-sm group relative hover:text-primary/80"
     >
       {children}
       <span className="absolute left-0 -bottom-1 h-0.5 w-0 bg-[#BFA46F] transition-all duration-300 ease-in-out group-hover:w-full"></span>
-    </a>
+    </Link>
   );
 };
 
